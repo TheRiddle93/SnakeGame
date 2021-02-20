76bdb4585f58ae917aeacf70fb010dbec29e3145
TheRiddle93 <34629376+TheRiddle93@users.noreply.github.com>
2021-02-20 03:10:41 +0100

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -9,8 +9,8 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
 
-    static final int SCREEN_WIDTH= 600;
-    static final int SCREEN_HEIGHT = 600;
+    static final int SCREEN_WIDTH= 650;
+    static final int SCREEN_HEIGHT = 650;
     static final int UNIT_SIZE = 15;
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
     static final int DELAY = 75;
